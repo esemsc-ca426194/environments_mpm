1282d3de89054cf310c79654d5699dfabe067452
esemsc-ca426194 <kh124@ic.ac.uk>
2024-10-06 19:42:19 +0100

    create solve_matrix_equation py file

--- scripts/smooth_image.py
+++ scripts/smooth_image.py
@@ -1,5 +1,20 @@
-from envtest import rand_array
+from envtest import smooth_image
+from scipy import misc
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
 
-shape = (3, 3)
+image = misc.ascent()
+sigma = 5
 
-print(rand_array(shape))
+def smooth_image(a, sigma = 1):
+    return gaussian_filter(a, sigma=sigma)
+
+smoothed_image = smooth_image(image, sigma)
+
+f = plt.figure()
+f.add_subplot(1, 2, 1)
+plt.imshow(image)
+f.add_subplot(1, 2, 2)
+plt.imshow(smoothed_image)
+plt.show(block = True)
+
